76cdd7998002c40bfbb1b6ef20b3f15a270b6f9b
youdongkwan <dkyou7@naver.com>
2019-07-31 19:30:12 +0900

    전체적인 구조 짜기

--- source/src/HiFive.vue
+++ source/src/HiFive.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="HiFive">
-    this is Hifive 
+    <Header></Header>
+    <Main></Main>
+    <Howto></Howto>
+    <Team></Team>
+    <Member/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-export default {
+import Header from './views/Header.vue';
+import Main from './views/Main.vue';
+import Howto from './views/HowTo.vue';
+import Team from './views/Team.vue';
+import Member from './views/Member.vue';
+import Footer from './views/Footer.vue';
 
+export default {
+  components:{
+    Header,
+    Main,
+    Howto,
+    Team,
+    Member,
+    Footer
+  }
 }
 </script>
 
