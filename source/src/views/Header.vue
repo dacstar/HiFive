c238bfb39e72d56a6e928eff224d48ca82831940
dkyou7 <dkyou7@naver.com>
2019-08-15 21:50:55 +0900

    Revert "[#13] 헤더메인 만들어보기"

--- source/src/views/Header.vue
+++ source/src/views/Header.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="Header">
     <Navbar></Navbar>
-    <HM></HM>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/HeaderContains/Navbar";
-import HM from "../components/HeaderContains/HeaderMain";
+
 export default {
   components:{
-    Navbar,HM
+    Navbar
   }
 }
 </script>
