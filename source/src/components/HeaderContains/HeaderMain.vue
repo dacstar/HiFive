4f2239370bea0f2a96b9cfb98735f1cedf6f5593
youdongkwan <dkyou7@naver.com>
2019-08-15 21:49:42 +0900

    [#13] 헤더메인 만들어보기

---
+++ source/src/components/HeaderContains/HeaderMain.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="headerMain">
+      this is headermain!!
+  </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+.headerMain{
+    position: relative;
+}
+</style>
