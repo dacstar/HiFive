1ea726f9a97fc0c4f8e5addff2680dbddb59d552
youdongkwan <dkyou7@naver.com>
2019-08-24 17:32:36 +0900

    [#24] 팀 화면 UI 개선중

--- source/src/views/Member.vue
+++ source/src/views/Member.vue
@@ -1,48 +1,20 @@
 <template>
   <div class="Member">
-    <h1 class="grey--text">하이파이브가 이 세상에 나오기까지,<span class="purple--text">노력한 사람들</span></h1>
-
-    <v-container class="my-5">
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
-          <v-card flat class="text-xs-center ma-3">
-            <v-responsive class="pt-4">
-              <v-avatar size="100" class="grey lighten-2">
-                <img :src="person.avatar">
-              </v-avatar>
-            </v-responsive>
-            <v-card-text>
-              <div>{{ person.name }}</div>
-              <div>{{ person.role }}</div>
-              <div>{{ person.intro}}</div>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-      </v-layout>
-
-    </v-container>
-
+    <MC></MC>
   </div>
 </template>
 
 <script>
+import MC from "../components/MemberContains";
   export default {
-    data() {
-      return {
-        team: [
-        { name: '유동관', role: '팀장', avatar: '/avatar-1.png', intro: '자기소개' },
-        { name: '노현우', role: 'Web developer', avatar: '/avatar-2.png', intro: '자기소개' },
-        { name: '신채원', role: 'Graphic designer', avatar: '/avatar-3.png', intro: '자기소개' },
-        { name: '박길준', role: 'Social media maverick', avatar: '/avatar-4.png', intro: '자기소개' }
-        ]
+      components:{
+        MC
       }
-    }
   }
 </script>
 
-<style>
-.Team{
-  height:100vh;
-    background: gold;
-}
+<style scoped>
+  .Member{
+    background: #fff;
+  }
 </style>
