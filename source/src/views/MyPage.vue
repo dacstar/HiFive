62507f75262cd32f10376684a02c61dde0aa11b1
노현우 <wkdus7h@naver.com>
2019-10-22 17:34:24 +0900

    complete mypage function

--- source/src/views/MyPage.vue
+++ source/src/views/MyPage.vue
@@ -1,90 +1,95 @@
 <template>
   <div class="app">
     <div class="container">
-       <div class="row">
-         <div><h1>내 방문장소</h1></div>
-         <template v-if="visit === true">
-      <template v-for="index in Lank">
-        <div :key=index
-        class="left">
-        <div style="float:left;">
-          <img src="https://post-phinf.pstatic.net/MjAxNzA2MDlfMjE3/MDAxNDk2OTg3ODIzODA5.MyG6GA71J-5DQE2UIusK6Zl9LyekZXHS4vzSkvSKRZkg.mTjbmTr-iLdD3idvOt5haiN90Iw0IV6Lb5hUkomEX7Eg.JPEG/%EA%B3%B5%EC%9C%A0.jpg?type=w1200" style="width:500px; height:500px; float:left;">
-        <div class="right">  
-          <h2 v-on:click="sendlocation(index.Y,index.X)">{{index.title}}</h2>
-          <h2>{{index.count}}</h2>
-        </div>  
-        </div>   
-          </div>
-        </template>
-      </template>
+      <div class="row">
+        <div>
+          <h1>내 방문장소</h1>
         </div>
-     </div>
+        <template v-if="visit === true">
+          <template v-for="place in Lank">
+            <div :key="place.storeName" class="left">
+              <div style="float:left;">
+                <img
+                  src="https://post-phinf.pstatic.net/MjAxNzA2MDlfMjE3/MDAxNDk2OTg3ODIzODA5.MyG6GA71J-5DQE2UIusK6Zl9LyekZXHS4vzSkvSKRZkg.mTjbmTr-iLdD3idvOt5haiN90Iw0IV6Lb5hUkomEX7Eg.JPEG/%EA%B3%B5%EC%9C%A0.jpg?type=w1200"
+                  style="width:500px; height:500px; float:left;"/>
+                <div class="right">
+                  <h2 v-on:click="sendlocation(place.location._lat, place.location._long)">{{ place.storeName }}</h2>
+                  <h2>{{ place.count }}</h2>
+                </div>
+              </div>
+            </div>
+          </template>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import db from "@/firebase";
+
 export default {
-  data(){
+  data() {
     return {
-     location:[
-       {title: "국민돼지국밥", Y : 127.29761976411594 , X : 36.349580368085796, count:10},
-       {title: "제주고기국수하르방", Y : 127.29726813490997 , X : 36.34851424918655, count:4},
-       {title: "동죽이네", Y : 127.29232251668599 , X : 36.347481083397234,  count:5},
-     ],
-     count:3,
-     Lank:[],
-     visit:false,
-    }
-  },methods:{
-    sendlocation:function(Y,X){
-      // alert(Y);
-      // alert(X);
-      this.$store.commit('doubleY',Y);
-      this.$store.commit('doubleX',X);
-      // alert("성공"+this.$store.state.Y);
+      Lank: [],
+      visit: true,
+      userVisitStore: []
+    };
+  },
+  methods: {
+    sendlocation(Y, X) {
+      this.$store.commit("doubleY", Y);
+      this.$store.commit("doubleX", X);
       this.$router.push("map");
     },
-    
-     /////순위 결정 데이터 오름차순 정렬 부분
-     update:function(){
-       var scope = this;
-      scope.visit=true;
-       ////여기서 location에 데이터를 넣어야 한다.
-        console.log("들왔오")
-        //location에 들어가는 예상 data(장소이름,좌표,하이파이브횟수)
-        //내림차순 정렬
-        //1등부터 3등 데이터 입력
-         console.log("들왔오2")
-        scope.Lank[0]=scope.location[0];
-        scope.Lank[1]=scope.location[1];
-        scope.Lank[2]=scope.location[2]; 
-        scope.Lank.sort(function(a,b){
-          return a.count >b.count ? -1 : 1;
-        });
-        console.log(scope.Lank)
-     }
-     
-  },mounted() {
-     this.update();
-  },
-}
 
+    update() {
+      var scope = this;
+      scope.visit = true;
+
+      // console.log('####', scope.userVisitStore[0].length);
+      for(var i=0; i < scope.userVisitStore[0].length; i++) {
+        scope.Lank[i] = scope.userVisitStore[0][i];
+      }
+
+      scope.Lank.sort(function(a,b){
+        return a.count > b.count ? -1 : 1;
+      });
+    },
+  },
+  mounted() {
+
+  },
+  created() {
+    var scope = this;
+    var user = "user1"; // 여기에 사용자 아이디 넣기
+    var docRef = db.collection("users").doc(user);
+    docRef.get().then(function(doc) {
+      if (doc.exists) {
+        scope.userVisitStore.push(doc.data().store);
+      } else {
+        console.log("No such document!");
+      }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+  },
+  watch : {
+    userVisitStore : function () {
+      this.update()
+    }
+  }
+}
 </script>
 
 <style>
 .left {
   width: 100%;
   height: 500px;
-  border: 3px solid #73AD21;
+  border: 3px solid #73ad21;
   padding: 10px;
 }
-.right{
+.right {
   float: right;
 }
-</style >
-}
-</script>
-
-<style>
-
 </style>
