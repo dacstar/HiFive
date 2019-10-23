0d2df7dc0c5a3e701ac12886f24233fa89856325
ChaewonShin <chaewonshin95@gmail.com>
2019-10-23 13:41:24 +0900

    S1P2251010-24 | CW | mypage_front_css done

--- source/src/views/MyPage.vue
+++ source/src/views/MyPage.vue
@@ -1,26 +1,31 @@
 <template>
-  <div class="app">
-    <div class="container">
-       <div class="row">
-         <div><h1>내 방문장소</h1></div>
-         <v-container>
 
-          
-              <div :key=index class="store_list">
-                <ul>
-                  <template v-if="visit === true">
-                    <template v-for="index in Lank">
-                      <li>
-                        <span class="number">{{index.idx}}</span>
-                        <span class="name" v-on:click="sendlocation(index.Y,index.X)">{{index.title}}</span>
-                        <span class="hifive_count">{{index.count}}</span>
-                        <img src="https://post-phinf.pstatic.net/MjAxNzA2MDlfMjE3/MDAxNDk2OTg3ODIzODA5.MyG6GA71J-5DQE2UIusK6Zl9LyekZXHS4vzSkvSKRZkg.mTjbmTr-iLdD3idvOt5haiN90Iw0IV6Lb5hUkomEX7Eg.JPEG/%EA%B3%B5%EC%9C%A0.jpg?type=w1200" style="width:50px; height:50px; float:left;">
-                        <!-- <span class="badge"><i class="fa fa-trophy" aria-hidden="true"></i></span> -->
-                      </li>
+       
+         
+         <v-container>
+           <div class="row">
+            <div class="my_hifive_info">내 하이파이브 정보</div>
+              <div class="hifive_rank">내가 방문한 하이파이브존 TOP3를 확인해보세요!</div>
+                <div :key=store class="store_list">
+                  <ul>
+                    <template v-if="visit === true">
+                      <template v-for="(store, index) in Lank">
+                        <li>
+                          <div>
+                            <span class="number">{{ index+1 }}</span>
+                            <img src="https://post-phinf.pstatic.net/MjAxNzA2MDlfMjE3/MDAxNDk2OTg3ODIzODA5.MyG6GA71J-5DQE2UIusK6Zl9LyekZXHS4vzSkvSKRZkg.mTjbmTr-iLdD3idvOt5haiN90Iw0IV6Lb5hUkomEX7Eg.JPEG/%EA%B3%B5%EC%9C%A0.jpg?type=w1200" style="width:50px; height:50px;">
+                            <span class="name" v-on:click="sendlocation(store.Y,store.X)">{{ store.title }}</span>
+                          </div>
+                          <div>
+                            
+                            <span class="hifive_count">하이파이브 지수 : {{ store.count }}</span>
+                            
+                          </div>
+                        </li>
+                      </template>
                     </template>
-                  </template>
-                </ul> 
-              </div>
+                  </ul> 
+                </div>
 
 
               <!-- <div :key=index class="left">
@@ -33,29 +38,28 @@
                 </div>   
               </div> -->
 
-
-            
+          </div>
         </v-container>
-        </div>
-     </div>
-  </div>
+        
+
 </template>
 
 <script>
 
 export default {
-  data(){
+  data() {
     return {
      location:[
-       {idx: 1, title: "국민돼지국밥", Y : 127.29761976411594 , X : 36.349580368085796, count:10},
-       {idx: 2, title: "제주고기국수하르방", Y : 127.29726813490997 , X : 36.34851424918655, count:4},
-       {idx: 3, title: "동죽이네", Y : 127.29232251668599 , X : 36.347481083397234,  count:5},
+       {title: "국민돼지국밥", Y : 127.29761976411594 , X : 36.349580368085796, count: 10},
+       {title: "제주고기국수하르방", Y : 127.29726813490997 , X : 36.34851424918655, count: 12},
+       {title: "동죽이네", Y : 127.29232251668599 , X : 36.347481083397234,  count: 25},
      ],
      count:3,
      Lank:[],
      visit:false,
     }
-  },methods:{
+  }, 
+  methods : {
     sendlocation:function(Y, X) {
       // alert(Y);
       // alert(X);
@@ -66,7 +70,7 @@ export default {
     },
     
      /////순위 결정 데이터 오름차순 정렬 부분
-     update:function(){
+     update : function() {
        var scope = this;
       scope.visit=true;
        ////여기서 location에 데이터를 넣어야 한다.
@@ -78,13 +82,13 @@ export default {
         scope.Lank[0]=scope.location[0];
         scope.Lank[1]=scope.location[1];
         scope.Lank[2]=scope.location[2]; 
-        scope.Lank.sort(function(a,b){
-          return a.count >b.count ? -1 : 1;
+        scope.Lank.sort(function(a, b) {
+          return a.count > b.count ? -1 : 1;
         });
         console.log(scope.Lank)
      }
      
-  },mounted() {
+  }, mounted() {
      this.update();
   },
 }
@@ -92,21 +96,16 @@ export default {
 </script>
 
 <style scoped>
-/* .left {
-  width: 100%;
-  height: 500px;
-  border: 3px solid #73AD21;
-  padding: 10px;
+.my_hifive_info {
+  font-size: 30px;
+  font-family: 'Jua', sans-serif;
 }
-.right{
-  float: right;
-} */
 
-body {
-    margin: 0;
-    padding: 0;
-    background: #ff3772;
-    font-family: sans-serif;
+.hifive_rank {
+  font-size: 15px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-family: 'Jua', sans-serif;
 }
 
 ul {
@@ -114,9 +113,11 @@ ul {
     width: 100%;
     padding: 10px;
     box-sizing: border-box;
-    background: rgba(0, 0, 0, .1);
-    box-shadow: inset 0 0 15px rgba(0,0,0,.2);
-    border-radius: 5px;
+    background: rgba(200, 25, 253, 0.1);
+    box-shadow: inset 0 0 15px rgba(198, 1, 247, 0.2);
+    /* border-block-color: purple; */
+    
+    /* border-radius: 5px; */
     overflow: hidden;
 }
 
@@ -127,6 +128,9 @@ ul li {
     color:black;
     margin: 5px 0;
     transition: .5s;
+    align-items: center;
+    justify-content: space-between;
+    font-family: 'Do Hyeon', sans-serif;
 }
 ul li:nth-child(1) {
     background: rgba(255, 255, 255, .6)
@@ -135,26 +139,33 @@ ul li:nth-child(2) {
     background: rgba(255, 255, 255, .4)
 }
 ul li:nth-child(3) {
-    background: rgba(255, 255, 255, .2)
+    background: rgba(255, 255, 255, .2);
 }
 
 ul li span:nth-child(1) {
     width: 30px;
+    margin-right: 20px;
+    /* margin-left: 10px; */
+    font-size: 1rem;
 }
 ul li span:nth-child(2) {
     width: 200px;
+    margin-left: 10px;
 }
 ul li span:nth-child(3) {
-    width: 100px;
     text-align: right;
+    margin-left: 10px;
+    padding-right: 30px;
 }
 ul li span:nth-child(4) {
     width: 70px;
     text-align: right;
+    align-items: center;
+
 }
 ul li:hover {
     transform:scale(1.06);
-    background: #ff3772;
+    background: #a120ec;
 }
 
 </style>
