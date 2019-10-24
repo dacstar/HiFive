b3e3616214d9702d27d514e985227e10bca723d3
HyeanWoo <46041638+HyeanWoo@users.noreply.github.com>
2019-10-24 10:26:34 +0900

    버그수정

--- source/src/main.js
+++ source/src/main.js
@@ -6,11 +6,11 @@ import { store } from './store'
 
 Vue.config.productionTip = false
 
+// 사용할 앱의 JavaScript 키를 설정해 주세요.
+Kakao.init('d5e4fbca80b67f3a6a75614545dd6b7b');
+
 new Vue({
   router,
   store,
   render: h => h(App),
 }).$mount('#app')
-
-// 사용할 앱의 JavaScript 키를 설정해 주세요.
-Kakao.init('d5e4fbca80b67f3a6a75614545dd6b7b');
