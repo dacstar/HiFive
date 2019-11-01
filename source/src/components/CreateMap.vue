517f33128e1ba15bb2a2ed7a2c7a3f4caa14f865
giljun <giljune91@gmail.com>
2019-11-02 00:16:59 +0900

    20191102|PGJ|add SweetAlert2

--- source/src/components/CreateMap.vue
+++ source/src/components/CreateMap.vue
@@ -5,11 +5,11 @@
 </template>
   
 <script>
-import { functions } from 'firebase';
-import { mapGetters, mapState } from 'vuex';
+import { functions } from "firebase";
+import { mapGetters, mapState } from "vuex";
 import db from "@/FirebaseService";
-import firebase from 'firebase/app';
-import { async } from 'q';
+import firebase from "firebase/app";
+import { async } from "q";
 
 export default {
   computed: {
@@ -21,22 +21,7 @@ export default {
     })
   },
   created() {
-    // var scope = this;
-    // fetchStoreList()
-    //   .then(function (response) {
-    //     console.log(response);
-    //     scope.stores = response.data;
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //     alert(error.message);
-    //   });
-    // 이걸 밑의 코드로 변형 가능하게 할 수 있습니다.
-    // store.js에서 묶음으로 확인 가능합니다.
-    // this.$store.dispatch('FETCH_STORES')
-  },
-  created() {
-    this.$store.dispatch('FETCH_STORES');
+    this.$store.dispatch("FETCH_STORES");
   },
   methods: {
     async create_customover() {
@@ -50,9 +35,9 @@ export default {
         center: new kakao.maps.LatLng(36.350185298428336, 127.29788497889939),
         level: 3 // 1 to 13
       };
-      var map = new kakao.maps.Map(mapContainer, mapOptions); // 지도 생성 
-
-      this.$store.commit('SET_MAP', map);
+      var map = new kakao.maps.Map(mapContainer, mapOptions); // 지도 생성
+      console.log(map);
+      this.$store.commit("SET_MAP", map);
 
       // 지도 확대 축소를 제어할 수 있는 줌 컨트롤을 생성
       var zoomControl = new kakao.maps.ZoomControl();
@@ -60,10 +45,11 @@ export default {
       // 지도가 확대 또는 축소되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
 
       var positions = this.$store.state.stores;
-      console.log(positions[0])
+      console.log(positions[0]);
 
       // Marker
-      var imageSrc = "https://www.iconsdb.com/icons/preview/violet/hand-cursor-xl.png"; // 마커 이미지 링크
+      var imageSrc =
+        "https://www.iconsdb.com/icons/preview/violet/hand-cursor-xl.png"; // 마커 이미지 링크
       var imageSize = new kakao.maps.Size(30, 30);
       var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
       var clickedOverlay = null;
@@ -71,79 +57,92 @@ export default {
       for (var i = 0; i < positions.length; i++) {
         var marker = new kakao.maps.Marker({
           map: map, // 마커를 표시할 지도
-          position: new kakao.maps.LatLng(positions[i].location.latitude, positions[i].location.longitude), // 마커를 표시할 위치
-          title: positions[i].storeName, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+          position: new kakao.maps.LatLng(
+            positions[i].location.latitude,
+            positions[i].location.longitude
+          ), // 마커를 표시할 위치
+          title: positions[i].storeName // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
         });
 
         var overlay = null;
-        var content = document.createElement('div');
-        content.className = 'wrap';
-        var info = document.createElement('div');
-        info.className = 'info';
+        var content = document.createElement("div");
+        content.className = "wrap";
+        var info = document.createElement("div");
+        info.className = "info";
         content.appendChild(info);
-        var title = document.createElement('div');;
-        title.className = 'title'
+        var title = document.createElement("div");
+        title.className = "title";
         title.innerHTML = positions[i].storeName;
         info.appendChild(title);
 
-        var body = document.createElement('div');
+        var body = document.createElement("div");
 
-        body.className = 'body';
+        body.className = "body";
         info.appendChild(body);
-        var close = document.createElement('div');
-        close.className = 'close';
+        var close = document.createElement("div");
+        close.className = "close";
         title.appendChild(close);
         overlay = new kakao.maps.CustomOverlay({
           content: content,
-          position: marker.getPosition(),
+          position: marker.getPosition()
         });
         close.onclick = makeOutListener(overlay);
-        kakao.maps.event.addListener(marker, 'click', makeOverListener(map, overlay));
+        kakao.maps.event.addListener(
+          marker,
+          "click",
+          makeOverListener(map, overlay)
+        );
       }
 
       function makeOverListener(map, overlay) {
-        return function () {
+        return function() {
           overlay.setMap(map);
         };
       }
 
-      // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다 
+      // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다
       function makeOutListener(overlay) {
-        return function () {
+        return function() {
           overlay.setMap(null);
         };
       }
     },
     make_info(data) {
       // 커스텀 오버레이에 표시할 컨텐츠 입니다
-      var result = '<div class="wrap">' +
+      var result =
+        '<div class="wrap">' +
         '    <div class="info">' +
-        '        <div class="title">' + data.title +
+        '        <div class="title">' +
+        data.title +
         '            <div class="close" @onclick="closeOverlay()" title="닫기"></div>' +
-        '        </div>' +
+        "        </div>" +
         '        <div class="body">' +
         '            <div class="img">' +
-        '                <img src="' + data.image_url + '" width="73" height="70">' +
-        '           </div>' +
+        '                <img src="' +
+        data.image_url +
+        '" width="73" height="70">' +
+        "           </div>" +
         '            <div class="desc">' +
-        '                <div class="ellipsis">' + data.address + '</div>' +
-        // '                <h6>'+ data.address + '</h6>' + 
-        // '                <div><a href="http://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' + 
-        '            </div>' +
-        '        </div>' +
-        '    </div>' +
-        '</div>';
+        '                <div class="ellipsis">' +
+        data.address +
+        "</div>" +
+        // '                <h6>'+ data.address + '</h6>' +
+        // '                <div><a href="http://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+        "            </div>" +
+        "        </div>" +
+        "    </div>" +
+        "</div>";
       return result;
     },
     closeOverlay() {
       alert("hi");
       overlay.setMap(null);
-    },
+    }
   },
   mounted() {
     this.create_customover();
   }
-}
+};
 </script>
 
 <style>
@@ -182,51 +181,6 @@ input[type="password"] {
   margin-top: 10px;
   margin-right: 10px;
   margin-bottom: 15px;
-}
-
-/* 버튼 */
-button {
-  background: #a120ec;
-  color: #fff !important;
-  border: none;
-  position: relative;
-  height: 30px;
-  font-size: 0.8em !important;
-  padding: 0 1 2em;
-  cursor: pointer;
-  transition: 800ms ease all;
-  outline: none;
-  font-family: "Do Hyeon", sans-serif;
-}
-
-button:hover {
-  background: #fff;
-  color: #a120ec;
-}
-
-button:before,
-button:after {
-  content: "";
-  position: absolute;
-  top: 0;
-  right: 0;
-  height: 2px;
-  width: 0;
-  background: #a120ec;
-  transition: 400ms ease all;
-}
-
-button:after {
-  right: inherit;
-  top: inherit;
-  left: 0;
-  bottom: 0;
-}
-
-button:hover:before,
-button:hover:after {
-  width: 100%;
-  transition: 800ms ease all;
 }
 
 /* 커스텀 오버레이 */
